6649768b4f995a8fca7d8c591edcdd9c08b6cbcb
Nicolas Cutrona <nicolascutrona@gmail.com>
2023-05-12 17:05:53 -0400

    Finalized Experiment Version

--- experiment.py
+++ experiment.py
@@ -2,16 +2,14 @@ import pandas as pd
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import ERNB
+import BARISTA
 import preprocess
 from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 plt.rcParams['font.family'] = 'serif'
 
 
-l1_penalties = [0.01, 0.03, 0.06, 0.09, 0.12]
-l2_penalties = [0.00001, 0.0001, 0.001, 0.005, 0.01, 0.05]
-def cross_val(X, y, learning_rate, l1_penalty, l2_penalty):
+def cross_val(training_samples, training_labels, scheme, learning_rate, convergence_constant, max_iterations, l1_penalty, l2_penalty):
     cross_accuracies = []
     kf = KFold(n_splits=5, random_state=None, shuffle=True)
     split = 0
@@ -22,23 +20,23 @@ def cross_val(X, y, learning_rate, l1_penalty, l2_penalty):
         X_test = X_test.reset_index(drop=True)
         y_train = y_train.reset_index(drop=True)
         y_test = y_test.reset_index(drop=True)
-        ernb = ERNB.ERNB()
-        ernb.fit(X_train, y_train, l1_penalty =l1_penalty, l2_penalty = l2_penalty, learning_rate = learning_rate, convergence_constant=1e-6)
-        ernb.predict(X_test, y_test)
-        cross_accuracies.append(ernb.accuracy)
+        barista = BARISTA.BARISTA()
+        barista.fit(training_samples, training_labels, scheme, learning_rate, convergence_constant, max_iterations, l1_penalty, l2_penalty)
+        barista.predict(X_test, y_test)
+        cross_accuracies.append(barista.accuracy)
         split += 1
         if(split == 1):
-            differences = ernb.norm_differnces()
+            differences = barista.norm_differnces()
     return np.mean(cross_accuracies), differences
 
 
-def experiment(X, y, learning_rate, l1_penalties, l2_penalties):
+def experiment(training_samples, training_labels, scheme, learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties):
     accuracies = []
     differences = []
     penalty_combo = []
     for i in range(len(l1_penalties)):
         for j in range(len(l2_penalties)):
-            accuracy, difference = cross_val(X, y, learning_rate, l1_penalties[i], l2_penalties[j])
+            accuracy, difference = cross_val(training_samples, training_labels, scheme, learning_rate, convergence_constant, max_iterations, l1_penalties[i], l2_penalties[j])
             accuracies.append(accuracy)
             differences.append(difference)
             penalty_combo.append([l1_penalties[i], l2_penalties[j]])
@@ -48,90 +46,128 @@ def experiment(X, y, learning_rate, l1_penalties, l2_penalties):
 
 
 
-#Loading The Data (Enter File Path)
-#breast_w = pd.read_csv('/Users/nicolascutrona/Desktop/RPNB Data/breast_w_synth.csv',index_col=[0])
-#breast_w = breast_w.drop("random", axis = 1)
-#breast_w = breast_w.drop("r3", axis = 1)
-'''breast_w = pd.read_csv('/Users/nicolascutrona/Desktop/RPNB Data/breast_w.csv')
-breast_w = preprocess.Preprocess(breast_w, "Class", [])
-X, y = breast_w.get_data()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
-X_train = X_train.reset_index(drop=True)
-X_test = X_test.reset_index(drop=True)
-y_train = y_train.reset_index(drop=True)
-y_test = y_test.reset_index(drop=True)
-ernb = ERNB.ERNB()
-ernb.fit(X_train, y_train, l1_penalty = 0.01, l2_penalty=0.001, learning_rate = 0.1, max_iterations=5000, convergence_constant=1e-6)
-ernb.predict(X_test, y_test)
-print("Testing Accuracy:", ernb.accuracy)'''
+#NUMERICAL SETTINGS
+scheme = 'FISTA'
+learning_rate = 0.1
+convergence_constant = 1e-6
+max_iterations = 5000
+l1_penalties = [0.01, 0.03, 0.06, 0.09, 0.12]
+l2_penalties = [0.00001, 0.0001, 0.001, 0.005, 0.01, 0.05]
 
 
-'''breast_w = pd.read_csv('/Users/nicolascutrona/Desktop/RPNB Data/breast_w.csv')
+#ISTA vs FISTA Experiments
+
+#=========================================================================================================================================================
+
+breast_w = pd.read_csv('/Users/nicolascutrona/Desktop/RPNB Data/breast_w.csv')
 breast_w = preprocess.Preprocess(breast_w, "Class", [])
 X, y = breast_w.get_data()
-best_parameters, experimental_accuracy, breast_w_differences = experiment(X,y, learning_rate = 0.1, l1_penalties=l1_penalties, l2_penalties = l2_penalties)
+
+best_parameters, experimental_accuracy, breast_fista_differences = experiment(X, y, scheme, learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties)
 print("breast-w 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameters)
+
+best_parameters, experimental_accuracy, breast_ista_differences = experiment(X, y, "ISTA", learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties)
+print("breast-w 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameters)
+
+#=========================================================================================================================================================
 
 statlog = pd.read_csv('/Users/nicolascutrona/Desktop/RPNB Data/statlog.csv')
 statlog = preprocess.Preprocess(statlog, "Target", ['age', 'resting_blood_pressure', 'serum_chol', 'max_hr', 'oldpeak'])
 X, y = statlog.get_data()
-best_parameter, experimental_accuracy, statlog_differences = experiment(X,y, learning_rate = 0.1, l1_penalties=l1_penalties, l2_penalties = l2_penalties)
-print("statlog 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameter)
+
+best_parameters, experimental_accuracy, statlog_fista_differences = experiment(X, y, scheme, learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties)
+print("statlog 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameters)
+
+best_parameters, experimental_accuracy, statlog_ista_differences = experiment(X, y, "ISTA", learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties)
+print("statlog 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameters)
+
+#=========================================================================================================================================================
 
 iris = pd.read_csv('/Users/nicolascutrona/Desktop/RPNB Data/iris.csv')
 iris = preprocess.Preprocess(iris, "variety", ['sepal.length', 'sepal.width', 'petal.length', 'petal.width'])
 X, y = iris.get_data()
-best_parameter, experimental_accuracy, iris_differences = experiment(X,y, learning_rate = 0.1, l1_penalties=l1_penalties, l2_penalties = l2_penalties)
-print("iris 5FCV Accuracy:", experimental_accuracy, "Best Penalty:", best_parameter)
+
+best_parameters, experimental_accuracy, iris_fista_differences = experiment(X, y, scheme, learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties)
+print("iris 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameters)
+
+best_parameters, experimental_accuracy, iris_ista_differences = experiment(X, y, "ISTA", learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties)
+print("iris 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameters)
+
+#=========================================================================================================================================================
 
 krkp = pd.read_csv('/Users/nicolascutrona/Desktop/RPNB Data/krkp.csv')
 krkp = preprocess.Preprocess(krkp, "36", [])
 X, y = krkp.get_data()
-best_parameter, experimental_accuracy, krkp_differences = experiment(X,y, learning_rate = 0.1, l1_penalties=l1_penalties, l2_penalties = l2_penalties)
-print("krkp 5FCV Accuracy:", experimental_accuracy, "Best Penalty:", best_parameter)
 
-mushroom = pd.read_csv('/Users/nicolascutrona/Desktop/RPNB Data/mushrooms.csv')
+best_parameters, experimental_accuracy, krkp_fista_differences = experiment(X, y, scheme, learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties)
+print("krkp 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameters)
+
+best_parameters, experimental_accuracy, krkp_ista_differences = experiment(X, y, "ISTA", learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties)
+print("krkp 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameters)
+
+#=========================================================================================================================================================
+
+mushroom = pd.read_csv('/Users/nicolascutrona/Desktop/RPNB Data/mushroom.csv')
 mushroom = preprocess.Preprocess(mushroom, "class", [])
 X, y = mushroom.get_data()
-best_parameter, experimental_accuracy, mushroom_differences = experiment(X,y, learning_rate = 0.1, l1_penalties=l1_penalties, l2_penalties = l2_penalties)
-print("mushroom 5FCV Accuracy:", experimental_accuracy, "Best Penalty:", best_parameter)'''
+
+best_parameters, experimental_accuracy, mushroom_fista_differences = experiment(X, y, scheme, learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties)
+print("mushroom 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameters)
+
+best_parameters, experimental_accuracy, mushroom_ista_differences = experiment(X, y, "ISTA", learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties)
+print("mushroom 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameters)
+
+#=========================================================================================================================================================
 
 zoo = pd.read_csv('/Users/nicolascutrona/Desktop/RPNB Data/zoo.csv')
 zoo = preprocess.Preprocess(zoo, "17", [])
 X, y = zoo.get_data()
-best_parameter, experimental_accuracy, zoo_differences = experiment(X,y, learning_rate = 0.1, l1_penalties=l1_penalties, l2_penalties = l2_penalties)
-print("zoo 5FCV Accuracy:", experimental_accuracy, "Best Penalty:", best_parameter)
+
+best_parameters, experimental_accuracy, zoo_fista_differences = experiment(X, y, scheme, learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties)
+print("zoo 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameters)
+
+best_parameters, experimental_accuracy, zoo_ista_differences = experiment(X, y, "ISTA", learning_rate, convergence_constant, max_iterations, l1_penalties, l2_penalties)
+print("zoo 5FCV Accuracy:", experimental_accuracy, "Best Penalty Combination:", best_parameters)
+
+#=========================================================================================================================================================
 
 
 
-'''#Convergence Plots
+#Convergence Plots
+
 fig, ax = plt.subplots(3, 2, sharex=False, sharey=False, constrained_layout=True, figsize=[7, 7])
 #fig.subplots_adjust(hspace=0.5)
-fig.suptitle('Semilog Plots of Weight Convergence', fontsize=12, fontweight='bold')
+fig.suptitle('Semilog Convergence', fontsize=12, fontweight='bold')
 
 # Plot the subplots
 # Plot 1
-ax[0,0].semilogy(breast_w_differences, 'b')
+ax[0,0].semilogy(breast_fista_differences, 'g')
+ax[0,0].semilogy(breast_ista_differences, 'b')
 ax[0,0].set_title('breast-w', fontsize=10)
 
 # Plot 2
-ax[0,1].semilogy(statlog_differences, 'b')
+ax[0,1].semilogy(statlog_fista_differences, 'g')
+ax[0,1].semilogy(statlog_ista_differences, 'b')
 ax[0,1].set_title('statlog', fontsize = 10)
 
 # Plot 3
-ax[1,0].semilogy(iris_differences, 'b')
+ax[1,0].semilogy(iris_fista_differences, 'g')
+ax[1,0].semilogy(iris_ista_differences, 'b')
 ax[1,0].set_title('iris', fontsize=10)
 
 # Plot 4
-ax[1,1].semilogy(krkp_differences, 'b')
+ax[1,1].semilogy(krkp_fista_differences, 'g')
+ax[1,1].semilogy(krkp_ista_differences, 'b')
 ax[1,1].set_title('kr-vs-kp', fontsize=10)
 
 # Plot 5
-ax[2,0].semilogy(mushroom_differences, 'b')
+ax[2,0].semilogy(mushroom_fista_differences, 'g')
+ax[2,0].semilogy(mushroom_ista_differences, 'b')
 ax[2,0].set_title('mushroom', fontsize = 10)
 
 # Plot 6
-ax[2,1].semilogy(zoo_differences, 'b')
+ax[2,1].semilogy(zoo_fista_differences, 'g')
+ax[2,1].semilogy(zoo_ista_differences, 'b')
 ax[2,1].set_title('zoo', fontsize = 10)
 
 # Adding a plot in the figure which will encapsulate all the subplots with axis showing only
@@ -141,4 +177,4 @@ fig.add_subplot(1, 1, 1, frame_on=False)
 plt.tick_params(labelcolor="none", bottom=False, left=False)
 
 # Adding the x-axis and y-axis labels for the bigger plot
-plt.show()'''
+plt.savefig("/Users/nicolascutrona/Desktop/convergence.png", dpi=300)
--- optimization.py
+++ optimization.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 
 class Optimization:
 
-    def __init__(self, learning_rate, training_samples, training_labels, max_iter, convergence_constant, l1_penalty, l2_penalty):
+    def __init__(self, training_samples, training_labels, scheme, learning_rate, max_iter, convergence_constant, l1_penalty, l2_penalty):
+        self.scheme = scheme
         self.learning_rate = learning_rate
         self.convergence_constant = convergence_constant
         self.max_iter = max_iter
@@ -32,14 +33,17 @@ class Optimization:
         self.ground_truths = self.get_ground_truth()
         self.posterior_probabability_distribution = self.posterior_probabilities()
         print("Initial Posterior Probability Distribution Computed...")
-        self.M = self.model_learning()
+        if(self.scheme == "ISTA"):
+            print("ISTA Scheme Selected")
+            self.M = self.ISTA()
+        else:
+            print("FISTA Scheme Selected (Default)")
+            self.M = self.FISTA()
 
 
     def initialize_weight_matrix(self):
         return np.ones(shape = (self.num_classes, self.num_attributes))
         
-
-
     def get_ground_truth(self):
         ground_truths = []
         for i in range(self.num_samples):
@@ -106,7 +110,7 @@ class Optimization:
         return ((1/self.num_samples) * (E)) + l2
 
     def soft_thresholding(self, update, learning_rate):
-        boundary = (learning_rate * self.l1_penalty) / 2
+        boundary = learning_rate * self.l1_penalty
         if((-1 * boundary) < update and update < boundary):
             return 0
         elif(update > boundary):
@@ -118,15 +122,6 @@ class Optimization:
         else:
             print("Proximal Descent Error --> will return 0")
             return 0 
-
-    '''def convergence_check(self, loss_old, loss_new):
-        converged = False
-        convergence_check = np.abs((loss_old - loss_new)/max(np.abs(loss_old), np.abs(loss_new), 1)) 
-        if(convergence_check < self.convergence_constant):
-            converged = True
-            return converged
-        else:
-            return converged'''
         
     def convergence_check(self, weight_1, weight_2):
         converged = False
@@ -144,7 +139,7 @@ class Optimization:
         Q = fy + (x-y).reshape(-1).dot(gradient.reshape(-1)) + 1/(2*learning_rate)*np.linalg.norm(x-y)**2 + l1
         return(Q)
 
-    def model_learning(self):
+    def FISTA(self):
         print("__Optimizing__...")
         weights = np.copy(self.weights)
         loss_values = []
@@ -223,3 +218,86 @@ class Optimization:
             #plt.plot(loss_values)
             #plt.show()
             return [self.weights, weight_collection]
+        
+
+    def ISTA(self):
+        print("__Optimizing__...")
+        weights = np.copy(self.weights)
+        loss_values = []
+        weight_collection = [weights]
+        iteration = 1
+        converged = False
+        loss = self.model_loss()
+        print("Initial Loss:", loss)
+        loss_values.append(loss)
+        #Hessian_obj = nd.Hessian(self.obj_func)
+        #min_eig_values = []
+        
+        while(converged != True and iteration <= self.max_iter):
+            learning_rate = np.copy(self.learning_rate)
+            while(learning_rate > 1e-8):
+                current_weights = np.copy(weights)
+                self.gradient_matrix = self.gradient(current_weights)
+                current_weights = current_weights - (learning_rate * self.gradient_matrix)
+                for i in range(self.num_classes):
+                    for j in range(self.num_attributes):
+                        current_weights[i][j] = self.soft_thresholding(current_weights[i][j], learning_rate)
+
+                self.weights = current_weights
+                self.posterior_probabability_distribution = self.posterior_probabilities()
+                loss = self.model_loss()
+
+                previous_loss_no_penalty = loss_values[-1]-self.l1_penalty*sum(abs(weights.reshape(-1)))
+                if(loss < self.quad_approx(current_weights, weights, previous_loss_no_penalty, learning_rate, self.gradient_matrix)):
+                    break
+                else:
+                    learning_rate *= 0.5
+                    
+            
+            weights = np.copy(self.weights)
+            weight_collection.append(self.weights)
+            gradient_norm = self.gradient_norm(self.gradient_matrix)
+            loss_values.append(loss)
+            converged = self.convergence_check(weight_collection[-1], weight_collection[-2])
+
+            #Convexity Analysis, Hessian PSD
+            #if(iteration%20 == 0):
+                #H = Hessian_obj(weight_matrix.flatten())
+                #min_eig = min(np.linalg.eig(H)[0])
+                #min_eig_values.append(min_eig)
+
+            print("Iteration:", iteration)
+            print("Learning Rate:", learning_rate)
+            print("L1_Penalty Term:", self.l1_penalty)
+            print("L2_Penalty Term:", self.l2_penalty)
+            print("Posterior Cache First Sample:", self.posterior_probabability_distribution[0])
+            print("\nWeight Matrix:", self.weights)
+            print("\nGradient Norm:", gradient_norm)
+            print("\nGradient Matrix:", self.gradient_matrix)
+            print("\nModel Loss:", loss)
+            print("==============================================================================================================================\n")
+            #if(iteration%20 ==0):
+                #print("Min Eigenvalue:", min_eig)
+            #print("Converged:", converged)
+            #print("=================================================================")
+            
+            iteration += 1
+            
+        self.loss = loss_values
+        if(converged):
+            print("_Optimization Successful_")
+            #plt.plot(loss_values)  
+            #plt.show()
+            return [self.weights, weight_collection]  
+        else:
+            print("_Optimization Failed_")
+            #plt.plot(loss_values)
+            #plt.show()
+            return [self.weights, weight_collection]
+
+
+
+
+
+
+
