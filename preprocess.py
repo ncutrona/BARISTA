caaadce0e4abbdcf8b580f735909c6eaef8403b1
Nicolas Cutrona <59042355+ncutrona@users.noreply.github.com>
2024-06-03 19:22:21 -0400

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -9,6 +9,7 @@ class Preprocess:
         self.dataframe = dataframe
         self.target_attribute = target_attribute
         self.continuous_attributes = continuous_attributes
+        self.impute_missing()
         self.discretize()
         self.make_str()
         self.samples, self.labels = self.get_data()
@@ -18,6 +19,20 @@ class Preprocess:
         labels = self.dataframe[self.target_attribute]
         samples = self.dataframe.drop(self.target_attribute, axis = 1)
         return samples, labels
+
+    def impute_missing(self):
+        columns = self.dataframe.columns
+        nan_result = list(self.dataframe.isnull().any())
+        for i in range(len(columns)):
+            if(nan_result[i] == True):
+                if(columns[i] in self.continuous_attributes):
+                    self.dataframe[columns[i]].fillna(self.dataframe[columns[i]].mean(), inplace=True)
+                else:
+                    self.dataframe[columns[i]].fillna(self.dataframe[columns[i]].mode()[0], inplace=True)
+            else:
+                continue
+        return self.dataframe
+
 
     def discretize(self):
         if(len(self.continuous_attributes)!= 0):
